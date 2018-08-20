471c05861f5017659c1b15382fb919ab1d2b6059
alexismalzieu <alex.malzieu@gmail.com>
2018-08-20 17:44:51 +0200

    using variables

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,18 +4,23 @@ console.log('App is running');
 
 //JSX
 
+var app = {
+  title: 'Indecision app',
+  subtitle: 'This is the indecision app'
+};
+
 var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Indecision App'
+    app.title
   ),
   React.createElement(
     'p',
     null,
-    'This is some info'
+    app.subtitle
   ),
   React.createElement(
     'ol',
@@ -30,6 +35,38 @@ var template = React.createElement(
       null,
       'Item two'
     )
+  )
+);
+
+var user = {
+  name: 'Alexis',
+  age: 21,
+  location: 'Lille'
+};
+
+var userName = 'Alexis';
+var userAge = 21;
+var userLocation = 'Lille';
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
   )
 );
 
--- src/app.js
+++ src/app.js
@@ -3,14 +3,37 @@ console.log('App is running');
 
 //JSX
 
+var app = {
+  title: 'Indecision app',
+  subtitle: 'This is the indecision app'
+}
+
 var template = (
   <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
     </ol>
+  </div>
+);
+
+var user = {
+  name: 'Alexis',
+  age: 21,
+  location: 'Lille'
+}
+
+var userName = 'Alexis';
+var userAge = 21;
+var userLocation = 'Lille'
+
+var template2 = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p>
   </div>
 );
 
