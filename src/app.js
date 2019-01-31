3d0424c7f84c2a0230bfe1ba70293688a6c6d770
alexismalzieu <alex.malzieu@gmail.com>
2019-01-31 13:50:17 +0100

    import export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('app.js is running!!!');
+//import './utils.js'
+// import { square, add } from './utils.js'
+//
+//
+// console.log('app.js is running!!!');
+//
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import {canDrink, isAdult} from './utils.js'
+
+console.log(canDrink(14));
+console.log(canDrink(17));
+
+console.log(isAdult(14));
+console.log(canDrink(19));
