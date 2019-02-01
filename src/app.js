dfdab92d92df94afb76a3dd38be83172224b4284
alexismalzieu <alex.malzieu@gmail.com>
2019-02-01 13:12:09 +0100

    default export

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-//import './utils.js'
-// import { square, add } from './utils.js'
-//
+// import substract, { square, add } from './utils.js'
 //
 // console.log('app.js is running!!!');
 //
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(substract(100,23));
 
-import {canDrink, isAdult} from './utils.js'
+import isSenior, {canDrink, isAdult} from './person.js'
 
-console.log(canDrink(14));
-console.log(canDrink(17));
+console.log('canDrink', canDrink(14));
+console.log('canDrink', canDrink(17));
 
-console.log(isAdult(14));
-console.log(canDrink(19));
+console.log('isAdult', isAdult(14));
+console.log('isAdult', isAdult(19));
+
+console.log('isSenior', isSenior(64));
+console.log('isSenior', isSenior(65));
---
+++ src/person.js
@@ -0,0 +1,8 @@
+
+export default age => age >= 65;
+
+const isAdult = age => age >= 18;
+
+const canDrink = age => age >= 16;
+
+export {isAdult, canDrink};
